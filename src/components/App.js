f75bf0ab8550d7fc2bed081c3236433044696360
Aragorn Elessar <mahmoud_gadallah@outlook.com>
2024-04-16 11:53:05 +0200

    feat: Display/hide new friend form

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
+import { useState } from 'react';
 import Button from './Button';
 import FormAddFriend from './FormAddFriend';
 import FormSplitBill from './FormSplitBill';
 import FriendsList from './FriendsList';
 
 export default function App() {
+  const [showAddFriend, setshowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setshowAddFriend(!showAddFriend);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend />
-        <Button>Add Friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? 'Close' : 'Add friend'}
+        </Button>
       </div>
 
       <FormSplitBill />
