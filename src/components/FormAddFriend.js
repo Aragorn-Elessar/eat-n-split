627583b17f69a77787e5f87ccd7bddd38c78bbbb
Aragorn Elessar <mahmoud_gadallah@outlook.com>
2024-04-17 11:55:32 +0200

    feat: Turn add friend form into controlled element

--- src/components/FormAddFriend.js
+++ src/components/FormAddFriend.js
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import Button from './Button';
 
 export default function FormAddFriend() {
+  const [friendName, setFriendName] = useState('');
+  const [friendImage, setFriendImage] = useState(
+    'https://ih1.redbubble.net/image.1446179621.9942/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg'
+  );
+
+  function handleFriendName(e) {
+    setFriendName(e.target.value);
+  }
+
+  function handleFriendImage(e) {
+    setFriendImage(e.target.value);
+  }
+
   return (
     <form className="form-add-friend">
       <label>👭 Friend name</label>
-      <input type="text" />
+      <input type="text" value={friendName} onChange={handleFriendName} />
 
       <label>🌄 Image URL</label>
-      <input type="text" />
+      <input type="text" value={friendImage} onChange={handleFriendImage} />
 
       <Button>Add</Button>
     </form>
